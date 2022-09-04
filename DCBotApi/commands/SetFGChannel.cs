e23512c3a1fc9a08ec796ad90bd1af7f5ca4c4f3
Axlii50 <34549741+Axlii50@users.noreply.github.com>
2022-09-05 00:10:13 +0200

    Set freegames channel id

---
+++ DCBotApi/commands/SetFGChannel.cs
@@ -0,0 +1,22 @@
+﻿using DCBotApi.Configuration;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCBotApi.commands
+{
+    internal class SetFGChannel : BaseCommandModule
+    {
+        [Command("SetFGChannel")]
+        public async Task SetFGChannelCommand(CommandContext ctx, ulong ChannelId)
+        {
+            Configuration.ConfigMenager.SetChannelId(ctx.Guild.Id, ChannelId, ChannelEnum.FGChannel);
+
+            await ctx.RespondAsync($"Kanał ustawiony");
+        }
+    }
+}
