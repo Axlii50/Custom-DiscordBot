208de1b73cd5001d247be53dff949bcaa8fa8595
Axlii50 <34549741+Axlii50@users.noreply.github.com>
2022-09-17 20:25:37 +0200

    Set language Command

---
+++ DCBotApi/commands/SetLanguage.cs
@@ -0,0 +1,24 @@
+﻿using DCBotApi.Configuration;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCBotApi.commands
+{
+    internal class SetLanguage : BaseCommandModule
+    {
+        [Command("SetLanguage")]
+        public async Task SetLanguageCommand(CommandContext ctx,Language.LangTypes type)
+        {
+            Configuration.ConfigMenager.SetLanguage(ctx.Guild.Id, type);
+
+            await ctx.RespondAsync(Language.LanguageMenager.GetLang(
+                ConfigMenager.GetLanguage(ctx.Guild.Id))
+                .CommandResponses[ctx.Command.Name.ToLower()]);
+        }
+    }
+}
